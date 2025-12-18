f0fe906faf9dc45e29f696a2dd7f5872c6744e17
lorenzocorallo <lorenzocorallo@users.noreply.github.com>
2025-12-18 17:44:00 +0000

    ci: deploy preview for PoliNetworkOrg/Rankings/branch/beta
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

---
+++ PoliNetworkOrg/Rankings/branch/beta/assets/index-DsmXYhO-.js
@@ -0,0 +1,1 @@
+import{c as i,N as c,j as n,X as m}from"./index-EgrkBakK.js";const a=s=>{const o=i.c(3),{error:r}=s;if(r instanceof c){let t;return o[0]===Symbol.for("react.memo_cache_sentinel")?(t=n.jsx("p",{children:"Ranking not found"}),o[0]=t):t=o[0],t}let e;return o[1]!==r?(e=n.jsx(m,{error:r}),o[1]=r,o[2]=e):e=o[2],e};export{a as errorComponent};
